5daec18da45049961e10d6597d4e7a531703aaf0
Everton <evrasouza@gmail.com>
2025-09-01 21:39:55 -0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,34 @@
+// cypress.config.js
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Remove logs nativos do console
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        },
+      });
+
+      // Filtra stderr
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium') {
+          launchOptions.args.push('--disable-logging');
+          launchOptions.args.push('--disable-gpu');
+        }
+        return launchOptions;
+      });
+    },
+    hideXHR: true,
+    env: {
+      brand: "canam",        // marca ativa
+      regions: [
+        { region: "ca", language: "en" }
+      ],
+      RUN_DESKTOP: true,
+      checkTitle: false,
+    },
+  },
+});
